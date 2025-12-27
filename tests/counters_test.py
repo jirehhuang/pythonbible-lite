39ce5eb3331715458ab2a1c9c4a85ef5d12d7542
Jireh Huang <33878358+jirehhuang@users.noreply.github.com>
2025-12-27 14:36:45 -0800

    fix: apply manual corrections to verse counts (#9)
    * fix: apply manual corrections to the verse counts
    
    * test: add test confirming manual corrections

--- tests/counters_test.py
+++ tests/counters_test.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import pytest
+
 import pythonbible as bible
 
 
@@ -236,3 +238,68 @@ def test_count_verses_in_new_testament() -> None:
 
     # Then the count is correct
     assert number_of_verses == 7957
+
+
+@pytest.mark.parametrize(
+    ("reference_text", "expected_verses"),
+    [
+        ("Genesis 31", 55),
+        ("Genesis 32", 32),
+        ("Exodus 7", 25),
+        ("Exodus 8", 32),
+        ("Exodus 21", 36),
+        ("Exodus 22", 31),
+        ("Leviticus 5", 19),
+        ("Leviticus 6", 30),
+        ("Numbers 16", 50),
+        ("Numbers 17", 13),
+        ("Numbers 29", 40),
+        ("Numbers 30", 16),
+        ("Deuteronomy 12", 32),
+        ("Deuteronomy 13", 18),
+        ("Deuteronomy 22", 30),
+        ("Deuteronomy 23", 25),
+        ("Deuteronomy 28", 68),
+        ("Deuteronomy 29", 29),
+        ("1 Samuel 21", 15),
+        ("1 Samuel 23", 29),
+        ("1 Samuel 24", 22),
+        ("1 Samuel 25", 44),
+        ("2 Samuel 18", 33),
+        ("2 Samuel 19", 43),
+        ("2 Kings 11", 21),
+        ("2 Kings 12", 21),
+        ("Ecclesiastes 4", 16),
+        ("Ecclesiastes 5", 20),
+        ("Song of Songs 6", 13),
+        ("Song of Songs 7", 13),
+        ("Jeremiah 8", 22),
+        ("Jeremiah 9", 26),
+        ("Ezekiel 20", 49),
+        ("Ezekiel 21", 32),
+        ("Hosea 1", 11),
+        ("Hosea 2", 23),
+        ("Hosea 11", 12),
+        ("Hosea 12", 14),
+        ("Hosea 13", 16),
+        ("Hosea 14", 9),
+        ("Jonah 1", 17),
+        ("Jonah 2", 10),
+        ("Micah 4", 13),
+        ("Micah 5", 15),
+        ("Nahum 1", 15),
+        ("Nahum 2", 13),
+        ("Zechariah 1", 21),
+        ("Zechariah 2", 13),
+        ("Malachi 3", 18),
+        ("Malachi 4", 6),
+    ],
+)
+def test_verses_in_select_chapters(reference_text: str, expected_verses: int) -> None:
+    """Test that select chapters have the correct number of verses based
+    on the KJV. The verses counts are manual corrections from the following
+    source.
+    """
+    # https://www.life-everlasting.net/pages/bible/Number%20of%20verses%20per%20chapter%20in%20Bible%20(KJV).pdf
+    reference: bible.NormalizedReference = bible.get_references(reference_text)[0]
+    assert bible.count_verses([reference]) == expected_verses
